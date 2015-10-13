a16190ebffc592ed09f06f0b4d2414bb182057b5
Sandy Lerman <sandy@presencelearning.com>
2015-10-12 22:47:16 -0700

    First pass at creating a default project

---
+++ project_name/requirements/base.py
@@ -0,0 +1,26 @@
+# stable Django
+django==1.8.5
+
+# Models
+django-model-utils==2.3.1
+
+# Configuration
+django-environ==0.3.0
+django-secure==1.0.1
+whitenoise==2.0.4
+
+# Unicode slugification
+unicode-slugify==0.1.3
+django-autoslug==1.8.0
+
+# Time zones support
+python-dateutil==2.4.2
+pytz==2015.6
+
+# Redis support
+django-redis==4.2.0
+redis==2.10.3
+
+# celery for async tasks
+celery==3.1.18
+
