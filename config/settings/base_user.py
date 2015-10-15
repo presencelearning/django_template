6091abdb21572d409e75eab865d5835967fa384b
Sandy Lerman <sandy@presencelearning.com>
2015-10-15 14:17:37 -0700

    Updates to the template

---
+++ config/settings/base_user.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+'''
+Base user settings
+
+- Add Django Debug Toolbar
+'''
+
+from .base_user import *  # noqa
+
+
+# django-debug-toolbar
+# ------------------------------------------------------------------------------
+MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+INSTALLED_APPS += ('debug_toolbar', )
+
+INTERNAL_IPS = ('127.0.0.1', '10.0.2.2',)
+
+DEBUG_TOOLBAR_CONFIG = {
+    'DISABLE_PANELS': [
+        'debug_toolbar.panels.redirects.RedirectsPanel',
+    ],
+    'SHOW_TEMPLATE_CONTEXT': True,
+}
